82545623864991ef7da8bcc7c715707629fcb68f
Mhdafh <mhdafh@tuta.io>
2022-10-11 11:20:04 +0530

    fix: pinned notes layout

--- components/NoteList.tsx
+++ components/NoteList.tsx
@@ -5,13 +5,30 @@ const NoteList = ({ noteList }) => {
 
   return (
     noteList.length !== 0 ?
-      (<div className={styles.container}>
-        {noteList.map((note) => {
-          return (
-            <Note id={note.id} key={note.id} title={note.title} body={note.body} isPinned={note.isPinned} />
-          )
-        })}
-      </div>) : (<div className={styles.emptyContainer}><h4 className={styles.emptyMessage}>Notes you add appear here</h4></div>)
+      (<>
+        <div className={styles.container}>
+          <h3 className={styles.title}>Pinned Notes</h3>
+          <div className={styles.noteListContainer}>
+            {noteList.map((note) => {
+              if (note.isPinned)
+                return (
+                  <Note id={note.id} key={note.id} title={note.title} body={note.body} isPinned={note.isPinned} />
+                )
+            })}
+          </div>
+        </div>
+        <div className={styles.container}>
+          <hr />
+          <div className={styles.noteListContainer}>
+            {noteList.map((note) => {
+              if (!note.isPinned)
+                return (
+                  <Note id={note.id} key={note.id} title={note.title} body={note.body} isPinned={note.isPinned} />
+                )
+            })}
+          </div>
+        </div></>
+      ) : (<div className={styles.emptyContainer}><h4 className={styles.emptyMessage}>Notes you add appear here</h4></div>)
   )
 }
 export default NoteList
