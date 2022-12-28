2f925b79774247d878d8d75395f8be46f1053711
mhdafh7 <muhammed.aflah@leegality.com>
2022-12-28 18:20:40 +0530

    feat: Login and signup ui

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,6 +1,44 @@
+import Link from 'next/link';
+import styles from './styles.module.scss';
+
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <div className={styles.container}>
+            <h2 className={styles.title}>Login</h2>
+      <div className={styles.card}>
+        <div className={styles.tape}></div>
+        <form className={styles.form}>
+          <div className={styles.formItem}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              className={styles.input}
+            />
+          </div>
+          <div className={styles.formItem}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              className={styles.input}
+            />
+          </div>
+          <button
+            type="submit"
+            className={styles.loginBtn}
+          >
+            Login
+          </button>
+        </form>
+      </div>
+      <h4 className={styles.bottomText}>
+        Don&apos;t have an account?&nbsp;
+        <span>
+          <Link href={'/signup'}>Sign up.</Link>
+        </span>
+      </h4>
+    </div>
+  );
+};
+export default Login;
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,6 +1,52 @@
+import Link from 'next/link';
+import styles from './styles.module.scss';
+
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
-export default Signup
+    <div className={styles.container}>
+      <h2 className={styles.title}>Create an account</h2>
+      <div className={styles.card}>
+        <div className={styles.tape}></div>
+        <form className={styles.form}>
+          <div className={styles.formItem}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              className={styles.input}
+            />
+          </div>
+          <div className={styles.formItem}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              className={styles.input}
+            />
+          </div>
+          <div className={styles.formItem}>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              className={styles.input}
+            />
+          </div>
+          <button
+            type="submit"
+            className={styles.signupBtn}
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+      <h4 className={styles.bottomText}>
+        Already have an account?&nbsp;
+        <span>
+          <Link href={'/login'}>Sign in.</Link>
+        </span>
+      </h4>
+    </div>
+  );
+};
+export default Signup;
