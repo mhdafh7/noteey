df9acb2fff4341a5e9236757425fbb715df54193
Mhdafh <mhdafh@tuta.io>
2023-02-20 23:50:52 +0530

    Added Login and signup pages

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,43 +1,47 @@
-import Link from 'next/link';
-import styles from './styles.module.scss';
+import Head from "next/head";
+import Link from "next/link";
+import { StarDark } from "../../components/Svgs/Abstract";
+import SigninIcon from "../../components/Svgs/SigninIcon";
+import styles from "./styles.module.scss";
 
 const Login = () => {
   return (
     <div className={styles.container}>
-            <h2 className={styles.title}>Login</h2>
+      <Head>
+        <title>Login</title>
+        <meta
+          name="description"
+          content="A Note taking Web Application made using Nextjs"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <span className={styles.titleWrapper}>
+        <StarDark />
+        <h2 className={styles.title}>Login</h2>
+        <SigninIcon />
+      </span>
       <div className={styles.card}>
-        <div className={styles.tape}></div>
         <form className={styles.form}>
           <div className={styles.formItem}>
             <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              className={styles.input}
-            />
+            <input type="email" name="email" className={styles.input} />
           </div>
           <div className={styles.formItem}>
             <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              className={styles.input}
-            />
+            <input type="password" name="password" className={styles.input} />
           </div>
-          <button
-            type="submit"
-            className={styles.loginBtn}
-          >
+          <button type="submit" className={styles.loginBtn}>
             Login
           </button>
         </form>
       </div>
-      <h4 className={styles.bottomText}>
+      <h4 className={styles.signUpText}>
         Don&apos;t have an account?&nbsp;
         <span>
-          <Link href={'/signup'}>Sign up.</Link>
+          <Link href={"/signup"}>Sign up.</Link>
         </span>
       </h4>
+      <h5 className={styles.branding}>Noteey</h5>
     </div>
   );
 };
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,28 +1,35 @@
-import Link from 'next/link';
-import styles from './styles.module.scss';
+import Head from "next/head";
+import Link from "next/link";
+import { StarLight } from "../../components/Svgs/Abstract";
+import SignupIcon from "../../components/Svgs/SignupIcon";
+import styles from "./styles.module.scss";
 
 const Signup = () => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Create an account</h2>
+      <Head>
+        <title>Create an account</title>
+        <meta
+          name="description"
+          content="A Note taking Web Application made using Nextjs"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <span className={styles.titleWrapper}>
+        <StarLight />
+        <h2 className={styles.title}>Create an account</h2>
+        <SignupIcon />
+      </span>
       <div className={styles.card}>
         <div className={styles.tape}></div>
         <form className={styles.form}>
           <div className={styles.formItem}>
             <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              className={styles.input}
-            />
+            <input type="email" name="email" className={styles.input} />
           </div>
           <div className={styles.formItem}>
             <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              className={styles.input}
-            />
+            <input type="password" name="password" className={styles.input} />
           </div>
           <div className={styles.formItem}>
             <label htmlFor="confirmPassword">Confirm Password</label>
@@ -32,20 +39,18 @@ const Signup = () => {
               className={styles.input}
             />
           </div>
-          <button
-            type="submit"
-            className={styles.signupBtn}
-          >
+          <button type="submit" className={styles.signupBtn}>
             Sign Up
           </button>
         </form>
       </div>
-      <h4 className={styles.bottomText}>
+      <h4 className={styles.loginText}>
         Already have an account?&nbsp;
         <span>
-          <Link href={'/login'}>Sign in.</Link>
+          <Link href={"/login"}>Login.</Link>
         </span>
       </h4>
+      <h5 className={styles.branding}>Noteey</h5>
     </div>
   );
 };
