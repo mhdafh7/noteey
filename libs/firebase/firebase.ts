f7f8638ef02ac196a2ac17959bd7905deeb84172
mhdafh7 <mhdafh@tuta.io>
2023-06-18 19:29:15 +0530

    remove firebase

--- libs/firebase/firebase.ts
+++ libs/firebase/firebase.ts
@@ -1,29 +1,29 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
 
-import { initializeApp } from "firebase/app"
-import { getFirestore } from '@firebase/firestore'
-import { getAuth } from 'firebase/auth'
-
-
-const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API,
-
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-}
+// import { initializeApp } from "firebase/app"
+// import { getFirestore } from '@firebase/firestore'
+// import { getAuth } from 'firebase/auth'
 
 
-const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+// const firebaseConfig = {
+//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API,
 
-// const analytics = getAnalytics(app)
+//     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+
+//     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+
+//     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+
+//     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+
+//     appId: process.env.NEXT_PUBLIC_APP_ID,
+
+//     measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+// }
+
+
+// const app = initializeApp(firebaseConfig)
+// export const auth = getAuth(app)
+// export const db = getFirestore(app)
+
+// // const analytics = getAnalytics(app)
