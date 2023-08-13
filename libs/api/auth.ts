b9d3fd8529c955fcbcd74c53fd6ca3c66d61fce0
mhdafh7 <mhdafh@tuta.io>
2023-08-13 19:24:58 +0530

    api for auth

---
+++ libs/api/auth.ts
@@ -0,0 +1,46 @@
+import { Note, User } from "@prisma/client";
+
+type FetcherProps = {
+  url: string;
+  method: string;
+  body: Partial<User> | Partial<Note>;
+  json?: boolean;
+};
+
+const fetcher = async ({ url, method, body, json = true }: FetcherProps) => {
+  const res = await fetch(url, {
+    method,
+    body: body && JSON.stringify(body),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("API Error");
+  }
+
+  if (json) {
+    const data = await res.json();
+    return data;
+  }
+};
+
+export const register = async (user: Partial<User>) => {
+  return fetcher({
+    url: "/api/register",
+    method: "POST",
+    body: user,
+    json: false,
+  });
+};
+
+export const signin = async (user: Partial<User>) => {
+  return fetcher({
+    url: "/api/signin",
+    method: "POST",
+    body: user,
+    json: false,
+  });
+};
