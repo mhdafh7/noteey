e1edf228316e5552f4f7416b4a23af1ded5b1aed
mhdafh7 <mhdafh@tuta.io>
2023-08-15 15:15:01 +0530

    feat: messages constants

---
+++ constants/messages.ts
@@ -0,0 +1,15 @@
+export const messages = {
+  auth: {
+    errors: {
+      login: "Login failed",
+      signup: "Signup failed",
+      logout: "Logout failed",
+      incorrect_credentials: "Incorrect email or password",
+    },
+    success: {
+      login: "Login successful",
+      signup: "Signup successful",
+      logout: "Logout successful",
+    },
+  },
+};
