55db6bb87545efcacbd2dd3c3335744d178930ac
mhdafh7 <mhdafh@tuta.io>
2023-09-02 18:56:46 +0530

    protect routes using middleware

--- middleware.ts
+++ middleware.ts
@@ -1,42 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
-import { jwtVerify } from "jose";
-const PUBLIC_FILE = /\.(.*)$/;
+export { default } from "next-auth/middleware";
 
-// other one is in a file with bcrypt which is not supported on edge runtimes
-const verifyJWT = async (jwt: string) => {
-  const { payload } = await jwtVerify(
-    jwt,
-    new TextEncoder().encode(process.env.JWT_SECRET)
-  );
-
-  return payload;
+export const config = {
+  matcher: ["/((?!signup|api|login).*)"],
 };
-
-export default async function middleware(req: NextRequest, res: NextResponse) {
-  const { pathname } = req.nextUrl;
-  if (
-    pathname.startsWith("/_next") ||
-    pathname.startsWith("/api") ||
-    pathname.startsWith("/static") ||
-    pathname.startsWith("/login") ||
-    pathname.startsWith("/signup") ||
-    PUBLIC_FILE.test(pathname)
-  ) {
-    return NextResponse.next();
-  }
-
-  const jwt = req.cookies.get(process.env.COOKIE_NAME as string);
-  if (!jwt) {
-    req.nextUrl.pathname = "/login";
-    return NextResponse.redirect(req.nextUrl);
-  }
-
-  try {
-    await verifyJWT(jwt.value);
-    return NextResponse.next();
-  } catch (e) {
-    console.error(e);
-    req.nextUrl.pathname = "/login";
-    return NextResponse.redirect(req.nextUrl);
-  }
-}
