f7b0c5c949efcec25b07b5307890d7c9c5554d49
mhdafh7 <mhdafh@tuta.io>
2023-09-03 11:12:09 +0530

    login form

---
+++ app/(auth)/login/form.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { useState } from "react";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { signIn } from "next-auth/react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { toast } from "react-toastify";
+
+import Spinner from "@/components/Spinner";
+import { loginSchema } from "@/constants/validation";
+import { messages } from "@/constants/messages";
+
+import styles from "./styles.module.scss";
+
+type LoginFormValues = {
+  email: string;
+  password: string;
+};
+
+const LoginForm = () => {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
+
+  const handleSubmit = async (values: LoginFormValues) => {
+    setLoading(true);
+    try {
+      const res = await signIn("credentials", {
+        redirect: false,
+        email: values.email,
+        password: values.password,
+        callbackUrl: callbackUrl,
+      });
+
+      setLoading(false);
+
+      if (!res?.error) {
+        router.push(callbackUrl);
+        toast.success(messages.auth.success.login);
+      } else {
+        toast.error(messages.auth.errors.incorrect_credentials);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(messages.auth.errors.login);
+    }
+  };
+  return (
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      onSubmit={(values) => handleSubmit(values)}
+      validationSchema={loginSchema}
+    >
+      {({ errors, touched }) => (
+        <Form className={styles.form}>
+          <div className={styles.formItem}>
+            <label htmlFor="email">Email</label>
+            <Field type="email" name="email" className={styles.input} />
+            <p className={styles.errorMessage}>
+              <ErrorMessage name="email" />
+            </p>
+          </div>
+          <div className={styles.formItem}>
+            <label htmlFor="password">Password</label>
+            <Field type="password" name="password" className={styles.input} />
+            <p className={styles.errorMessage}>
+              <ErrorMessage name="password" />
+            </p>
+          </div>
+          <button type="submit" className={styles.loginBtn}>
+            {loading ? <Spinner scale={0.8} /> : "Login"}
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default LoginForm;
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,34 +1,11 @@
-"use client";
-
 import Link from "next/link";
-import { ErrorMessage, Field, Form, Formik } from "formik";
-import { StarDark } from "@/components/Svgs/Abstract";
-import styles from "./styles.module.scss";
-import { toast } from "react-toastify";
-import { useRouter } from "next/navigation";
-import Spinner from "@/components/Spinner";
-import { login } from "@/libs/api/auth";
-import { messages } from "@/constants/messages";
-import { loginSchema } from "@/constants/validation";
 
-type LoginFormValues = {
-  email: string;
-  password: string;
-};
+import { StarDark } from "@/components/Svgs/Abstract";
+import LoginForm from "./form";
+
+import styles from "./styles.module.scss";
 
 const Login = () => {
-  const router = useRouter();
-
-  const handleSubmit = async (values: LoginFormValues) => {
-    try {
-      await login(values);
-      toast.success(messages.auth.success.login);
-      router.push("/");
-    } catch (error) {
-      console.error(error);
-      toast.error(messages.auth.errors.login);
-    }
-  };
 
   return (
     <>
@@ -37,40 +14,7 @@ const Login = () => {
         <h2 className={styles.title}>Login</h2>
       </span>
       <div className={styles.card}>
-        <Formik
-          initialValues={{
-            email: "",
-            password: "",
-          }}
-          onSubmit={(values) => handleSubmit(values)}
-          validationSchema={loginSchema}
-        >
-          {({ errors, touched, isSubmitting }) => (
-            <Form className={styles.form}>
-              <div className={styles.formItem}>
-                <label htmlFor="email">Email</label>
-                <Field type="email" name="email" className={styles.input} />
-                <p className={styles.errorMessage}>
-                  <ErrorMessage name="email" />
-                </p>
-              </div>
-              <div className={styles.formItem}>
-                <label htmlFor="password">Password</label>
-                <Field
-                  type="password"
-                  name="password"
-                  className={styles.input}
-                />
-                <p className={styles.errorMessage}>
-                  <ErrorMessage name="password" />
-                </p>
-              </div>
-              <button type="submit" className={styles.loginBtn}>
-                {isSubmitting ? <Spinner scale={0.8} /> : <p>Login</p>}
-              </button>
-            </Form>
-          )}
-        </Formik>
+        <LoginForm />
       </div>
       <h4 className={styles.signUpText}>
         Don&apos;t have an account?&nbsp;
