f7cd4ead0b255794fe3df14515c69aabe53f7e86
mhdafh7 <mhdafh@tuta.io>
2023-09-03 11:12:36 +0530

    signup route

---
+++ app/api/auth/signup/route.ts
@@ -0,0 +1,55 @@
+import { messages } from "@/constants/messages";
+import { prisma } from "@/libs/db";
+import { Prisma } from "@prisma/client";
+import { hash } from "bcrypt";
+import httpStatus from "http-status";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: (await req.json()).email.toLowerCase(),
+      },
+    });
+
+    if (existingUser) {
+      return new NextResponse(
+        JSON.stringify({
+          status: "error",
+          message: messages.auth.errors.user_already_exists,
+        }),
+        { status: httpStatus.BAD_REQUEST }
+      );
+    }
+
+    const { firstName, lastName, email, password } =
+      (await req.json()) as Prisma.UserCreateInput;
+    const hashed_password = await hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email: email.toLowerCase(),
+        password: hashed_password,
+      },
+    });
+
+    return NextResponse.json({
+      user: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      },
+    });
+  } catch (error: any) {
+    return new NextResponse(
+      JSON.stringify({
+        status: "error",
+        message: error.message,
+      }),
+      { status: httpStatus.BAD_REQUEST }
+    );
+  }
+}
