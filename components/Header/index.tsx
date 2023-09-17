de6856a2c27a7efd64d8e9ce41253da280fc4796
mhdafh7 <mhdafh@tuta.io>
2023-09-18 00:42:54 +0530

    drawer menu button

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { Menu } from "react-feather";
 import LogoutButton from "./LogoutButton";
 
 import styles from "./styles.module.scss";
@@ -5,6 +6,9 @@ import styles from "./styles.module.scss";
 const Header = () => {
   return (
     <div className={styles.container}>
+      <button className={styles.menuButton}>
+        <Menu />
+      </button>
       <div className={styles.logo}>Noteey</div>
       {/* TODO search */}
       <LogoutButton />
