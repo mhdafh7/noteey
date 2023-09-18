5f8b59615ec3a98a6f96f8bd9d5215b4fd5ddb5d
mhdafh7 <mhdafh@tuta.io>
2023-09-18 19:53:30 +0530

    active tab

--- components/DrawerMenu/index.tsx
+++ components/DrawerMenu/index.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { Edit2, Trash } from "react-feather";
-import { useDrawerStore } from "@/store/drawer";
+import { DrawerState, useDrawerStore } from "@/store/drawer";
 
 import styles from "./styles.module.scss";
 
 const DrawerMenu = () => {
   const isDrawerOpen = useDrawerStore((state) => state.isDrawerOpen);
+  const activeTab = useDrawerStore((state) => state.activeTab);
+
   const drawerClass = isDrawerOpen
     ? `${styles.container} ${styles.open}`
     : styles.container;
@@ -14,11 +16,23 @@ const DrawerMenu = () => {
   return (
     <nav className={drawerClass}>
       <ul className={styles.list}>
-        <li>
+        <li
+          className={
+            activeTab === DrawerState.HOME
+              ? `${styles.activeTab}`
+              : `${styles.listItem}`
+          }
+        >
           <Edit2 fill="#272727" />
           <p>Notes</p>
         </li>
-        <li>
+        <li
+          className={
+            activeTab === DrawerState.TRASH
+              ? `${styles.activeTab}`
+              : `${styles.listItem}`
+          }
+        >
           <Trash fill="#272727" />
           <p>Trash</p>
         </li>
--- store/drawer.ts
+++ store/drawer.ts
@@ -1,11 +1,20 @@
 import { create } from "zustand";
 
+export enum DrawerState {
+  HOME = "/",
+  TRASH = "/trash",
+}
+
 type DrawerStore = {
   isDrawerOpen: boolean;
   toggleDrawer: () => void;
+  activeTab: DrawerState;
+  setActiveTab: (state: DrawerState) => void;
 };
 
 export const useDrawerStore = create<DrawerStore>((set) => ({
   isDrawerOpen: false,
   toggleDrawer: () => set((state) => ({ isDrawerOpen: !state.isDrawerOpen })),
+  activeTab: DrawerState.HOME,
+  setActiveTab: (state) => set(() => ({ activeTab: state })),
 }));
