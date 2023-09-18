a5f650066329933c84d788e1f0b7d420ae5bfd6d
mhdafh7 <mhdafh@tuta.io>
2023-09-18 14:35:13 +0530

    update home page layout

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,14 +2,17 @@ import Header from "@/components/Header";
 import TabBar from "@/components/TabBar";
 import NoteList from "@/components/NoteList";
 import MobileBottomNavbar from "@/components/MobileBottomNavbar";
+import DrawerMenu from "@/components/DrawerMenu";
+
 import styles from "./Home.module.scss";
 
 export default async function Home() {
   return (
     <div className={styles.container}>
       <Header />
-      <TabBar />
+      <DrawerMenu />
       <main className={styles.main}>
+        <TabBar />
         <NoteList />
         <MobileBottomNavbar />
       </main>
