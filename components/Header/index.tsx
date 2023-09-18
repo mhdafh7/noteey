6705e8e457c0ec05fd69a7ad7c585c5f9fd41d03
mhdafh7 <mhdafh@tuta.io>
2023-09-18 13:41:12 +0530

    include drawer button; semantic html

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,18 +1,18 @@
-import { Menu } from "react-feather";
+import DrawerButton from "./DrawerButton";
 import LogoutButton from "./LogoutButton";
 
 import styles from "./styles.module.scss";
 
 const Header = () => {
   return (
-    <div className={styles.container}>
-      <button className={styles.menuButton}>
-        <Menu />
-      </button>
-      <div className={styles.logo}>Noteey</div>
-      {/* TODO search */}
-      <LogoutButton />
-    </div>
+    <>
+      <header className={styles.container}>
+        <DrawerButton />
+        <div className={styles.logo}>Noteey</div>
+        {/* TODO search */}
+        <LogoutButton />
+      </header>
+    </>
   );
 };
 export default Header;
