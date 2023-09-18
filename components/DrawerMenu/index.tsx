f0fc19a9e9c2d03483ca8ba194208326aecbbc03
mhdafh7 <mhdafh@tuta.io>
2023-09-18 19:54:35 +0530

    switch pages from drawer

--- components/DrawerMenu/index.tsx
+++ components/DrawerMenu/index.tsx
@@ -4,40 +4,51 @@ import { Edit2, Trash } from "react-feather";
 import { DrawerState, useDrawerStore } from "@/store/drawer";
 
 import styles from "./styles.module.scss";
+import Link from "next/link";
 
 const DrawerMenu = () => {
-  const isDrawerOpen = useDrawerStore((state) => state.isDrawerOpen);
-  const activeTab = useDrawerStore((state) => state.activeTab);
+  const { activeTab, isDrawerOpen, toggleDrawer } = useDrawerStore();
 
   const drawerClass = isDrawerOpen
     ? `${styles.container} ${styles.open}`
     : styles.container;
 
   return (
-    <nav className={drawerClass}>
-      <ul className={styles.list}>
-        <li
-          className={
-            activeTab === DrawerState.HOME
-              ? `${styles.activeTab}`
-              : `${styles.listItem}`
-          }
-        >
-          <Edit2 fill="#272727" />
-          <p>Notes</p>
-        </li>
-        <li
-          className={
-            activeTab === DrawerState.TRASH
-              ? `${styles.activeTab}`
-              : `${styles.listItem}`
-          }
-        >
-          <Trash fill="#272727" />
-          <p>Trash</p>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className={drawerClass}>
+        <ul className={styles.list}>
+          <Link
+            href={"/"}
+            className={
+              activeTab === DrawerState.HOME
+                ? `${styles.activeTab}`
+                : `${styles.listItem}`
+            }
+          >
+            <Edit2 fill="#272727" />
+            <p>Notes</p>
+          </Link>
+          <Link
+            href={"/trash"}
+            className={
+              activeTab === DrawerState.TRASH
+                ? `${styles.activeTab}`
+                : `${styles.listItem}`
+            }
+          >
+            <Trash fill="#272727" />
+            <p>Trash</p>
+          </Link>
+        </ul>
+      </nav>
+      {isDrawerOpen ? (
+        <div
+          className={styles.overlay}
+          aria-hidden
+          onClick={toggleDrawer}
+        ></div>
+      ) : null}
+    </>
   );
 };
 export default DrawerMenu;
