a1df96e2974e8500b7a56545645c8b796e698b2b
mhdafh7 <mhdafh@tuta.io>
2023-09-19 23:15:01 +0530

    dynamic header logo based on url

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,23 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import DrawerButton from "./DrawerButton";
 import LogoutButton from "./LogoutButton";
 
 import styles from "./styles.module.scss";
+import { UrlEnum } from "@/enum/url";
 
 const Header = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
+
+  const logoText = currentPath === UrlEnum.TRASH ? "Trash" : "Noteey";
+
   return (
     <>
       <header className={styles.container}>
         <DrawerButton />
-        <div className={styles.logo}>Noteey</div>
+        <div className={styles.logo}>{logoText}</div>
         {/* TODO search */}
         <LogoutButton />
       </header>
