56ede9d66ae6824677117c220f21f6c752d23fc5
mhdafh7 <mhdafh@tuta.io>
2023-09-21 00:11:05 +0530

    loading animation on logout button

--- components/Header/LogoutButton.tsx
+++ components/Header/LogoutButton.tsx
@@ -1,17 +1,28 @@
 "use client";
 
+import { useState } from "react";
 import { signOut } from "next-auth/react";
 import { LogOut } from "react-feather";
+import RoundSpinner from "../Loaders/RoundSpinner";
+
 import styles from "./styles.module.scss";
 
 const LogoutButton = () => {
+  const [loading, setLoading] = useState(false);
   const handleSignOut = () => {
+    setLoading(true);
     signOut();
   };
   return (
     <button onClick={handleSignOut} className={styles.logOutBtn}>
-      <p>Logout</p>
-      <LogOut size={18} />
+      {loading ? (
+        <RoundSpinner scale={0.4} />
+      ) : (
+        <>
+          <p>Logout</p>
+          <LogOut size={18} />
+        </>
+      )}
     </button>
   );
 };
