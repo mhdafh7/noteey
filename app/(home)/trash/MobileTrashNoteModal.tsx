1124044336367855cf8f7fc7492fa4dd5850af28
mhdafh7 <mhdafh@tuta.io>
2023-09-20 21:51:09 +0530

    integrate delete and restore apis

--- app/(home)/trash/MobileTrashNoteModal.tsx
+++ app/(home)/trash/MobileTrashNoteModal.tsx
@@ -1,21 +1,41 @@
 "use client";
 
+import { useEffect } from "react";
 import { ArrowLeft } from "react-feather";
 import { useTrashStore } from "@/store/trash";
+import NoteMutation from "@/libs/hooks/mutations/note";
+import RoundSpinner from "@/components/Loaders/RoundSpinner";
+
 import styles from "./trash.module.scss";
 
 const MobileNoteModal = () => {
   const { currentTrashNote, setIsTrashNoteModalOpen } = useTrashStore();
-  const closeModal = () => {
+
+  const restoreNoteMutation = NoteMutation.useRestoreNote(currentTrashNote.id);
+  const deleteNoteMutation = NoteMutation.useDeleteNoteFromTrash(
+    currentTrashNote.id
+  );
+
+  const handleClose = () => {
     setIsTrashNoteModalOpen(false);
   };
-  const handleRestore = () => {};
-  const handleDelete = () => {};
+  const handleRestore = () => {
+    restoreNoteMutation.mutate();
+  };
+  const handleDelete = () => {
+    deleteNoteMutation.mutate();
+  };
+
+  useEffect(() => {
+    if (restoreNoteMutation.isSuccess || deleteNoteMutation.isSuccess) {
+      handleClose();
+    }
+  }, [restoreNoteMutation.isSuccess, deleteNoteMutation.isSuccess]);
 
   return (
     <div className={styles.mobileTrashNoteModal}>
       <nav className={styles.topBar}>
-        <button className={styles.back} onClick={closeModal}>
+        <button className={styles.back} onClick={handleClose}>
           <ArrowLeft size={32} />
         </button>
       </nav>
@@ -25,10 +45,18 @@ const MobileNoteModal = () => {
       </div>
       <div className={styles.buttons}>
         <button className={styles.restore} onClick={handleRestore}>
-          Restore
+          {restoreNoteMutation.isLoading ? (
+            <RoundSpinner scale={0.4} />
+          ) : (
+            "Restore"
+          )}
         </button>
         <button className={styles.delete} onClick={handleDelete}>
-          Delete Forever
+          {restoreNoteMutation.isLoading ? (
+            <RoundSpinner scale={0.4} />
+          ) : (
+            "Delete forever"
+          )}
         </button>
       </div>
     </div>
--- app/(home)/trash/TrashNoteModal.tsx
+++ app/(home)/trash/TrashNoteModal.tsx
@@ -1,16 +1,37 @@
 "use client";
 
+import { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { useTrashStore } from "@/store/trash";
+import NoteMutation from "@/libs/hooks/mutations/note";
+import RoundSpinner from "@/components/Loaders/RoundSpinner";
 
 import styles from "./trash.module.scss";
 
 const TrashNoteModal = () => {
   const { currentTrashNote, setIsTrashNoteModalOpen } = useTrashStore();
 
+  const restoreNoteMutation = NoteMutation.useRestoreNote(currentTrashNote.id);
+  const deleteNoteMutation = NoteMutation.useDeleteNoteFromTrash(
+    currentTrashNote.id
+  );
+
   const handleClose = () => {
     setIsTrashNoteModalOpen(false);
   };
+
+  const handleRestore = () => {
+    restoreNoteMutation.mutate();
+  };
+  const handleDelete = () => {
+    deleteNoteMutation.mutate();
+  };
+
+  useEffect(() => {
+    if (restoreNoteMutation.isSuccess || deleteNoteMutation.isSuccess) {
+      handleClose();
+    }
+  }, [restoreNoteMutation.isSuccess, deleteNoteMutation.isSuccess]);
 
   return ReactDOM.createPortal(
     <>
@@ -21,8 +42,20 @@ const TrashNoteModal = () => {
           <p className={styles.description}>{currentTrashNote.description}</p>
         </div>
         <div className={styles.buttons}>
-          <button className={styles.restore}>Restore</button>
-          <button className={styles.delete}>Delete</button>
+          <button className={styles.restore} onClick={handleRestore}>
+            {restoreNoteMutation.isLoading ? (
+              <RoundSpinner scale={0.4} />
+            ) : (
+              "Restore"
+            )}
+          </button>
+          <button className={styles.delete} onClick={handleDelete}>
+            {restoreNoteMutation.isLoading ? (
+              <RoundSpinner scale={0.4} />
+            ) : (
+              "Delete forever"
+            )}
+          </button>
         </div>
       </aside>
     </>,
